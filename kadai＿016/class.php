32e216db1117dc2963dca1defcf6149764268257
FujishimaGitKatsuya <fujikatz@gmail.com>
2025-05-29 19:40:29 +0900

    Update class.php

--- kadai＿016/class.php
+++ kadai＿016/class.php
@@ -7,37 +7,35 @@
 </head>
 
 <body>
-    <p>
-        <?php
-        // 食品のクラスを定義する
-        class Food {
-            // プロパティを定義する
-            private $name;
-            private $price;  
+  <p>
+    <?php
+      // 食品のクラスを定義する
+      class Food {
+        // プロパティを定義する
+        private $name;
+        private $price;  
         
-            // コンストラクタを定義する
-            public function __construct(string $name, int $price) {
-                $this->name = $name;
-                $this->price = $price;
-            }
-
-            // メソッドを定義する
-            public function set_name(string $name) {
-                $this->name = $name;
-            }
-
-         public function show_name() {
-                echo $this->name . '<br>';
-            }
-
-            public function get_price() {
-                return $this->price;
-            }   
+        // コンストラクタを定義する
+        public function __construct(string $name, int $price) {
+          $this->name = $name;
+          $this->price = $price;
         }
 
+        // メソッドを定義する
+        
+        public function show_name() {
+          echo $this->name;
+        }
+
+        public function show_price() {
+          echo $this->price;
+        }   
+      }
+
         // 食品クラスのインスタンスを作成し、メソッドを呼び出す
-        $potato = new Food('', 250);
-        $potato->show_name() ;
+        $potato = new Food('potato', 250);
+        
+        
         
 
         // インスタンスの各プロパティの値を出力する
@@ -67,20 +65,26 @@
             public function show_height() {
                 echo $this->height . '<br>';
             }
-
-            public function get_height() {
-                return $this->height;
-            }
+           
         }
 
         // 動物クラスのインスタンスを作成し、メソッドを呼び出す
-        $dog = new Animal('dog', 60, 5000);        
+        $dog = new Animal('dog', 60, 5000); 
+               
 
         // インスタンスの各プロパティの値を出力する
-        print_r($dog);       
-            echo '<br>';         
-           
-         ?>
-    </p>
+            print_r($dog);          
+            echo '<br>';       
+       
+        $potato->show_price();
+        echo '<br>'; 
+        $dog->show_height();
+
+    //$potato = new Food('potato', 250);
+    //$dog = new Animal('dog', 60, 5000);
+
+   ?>
+</p>
 </body>
 </html>
+ 
